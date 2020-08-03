15cde52483aefc72ca3e94cbcc4d9abc5dde8b1d
lcver <crackover1092@gmail.com>
2020-08-03 16:26:38 +0700

    select data

---
+++ [2]MySQL/demo_db_select.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+var conn = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"Lucver1092",
+    database:"learning_nodejs"
+});
+
+conn.connect(function(err) {
+    if(err) throw err;
+    conn.query("Select * from customers", function(err, result, fields) {
+        if(err) throw err;
+        console.log(result);
+    })
+})
